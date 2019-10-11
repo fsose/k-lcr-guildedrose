ff14bc21087a6ca4700f77c7a684345f08b76e2c
Partho P. Das <partho.das@edmi.com.au>
2019-10-11 13:32:46 +1000

    Step 7 - Refactoring - Replace conditionals with dictionary.

--- GuildedRose/GuildedRose.cs
+++ GuildedRose/GuildedRose.cs
@@ -1,9 +1,17 @@
+using System;
 using System.Collections.Generic;
 
 namespace GuildedRose
 {
     public class GildedRose
     {
+        private static readonly IReadOnlyDictionary<string, Action<Item>> itemUpdaters = new Dictionary<string, Action<Item>>
+        {
+            ["Aged Brie"] = DoUpdateQualityForAgedBrie,
+            ["Backstage passes to a TAFKAL80ETC concert"] = DoUpdateQualityForBackStagepasses,
+            ["Sulfuras, Hand of Ragnaros"] = DoUpdateQualityForSulfuras,
+        };
+
         IList<Item> Items;
         public GildedRose(IList<Item> Items)
         {
@@ -20,79 +28,89 @@ namespace GuildedRose
 
         private static void DoUpdateQuality(Item item)
         {
-            if (item.Name == "Aged Brie")
+            if (itemUpdaters.ContainsKey(item.Name))
+            {
+                itemUpdaters[item.Name](item);
+            }
+            else
+            {
+                DoUpdateQualityForEverythingElse(item);
+            }
+        }
+
+        #region Item updaters
+
+        private static void DoUpdateQualityForEverythingElse(Item item)
+        {
+            if (item.Quality > 0)
+            {
+                item.Quality = item.Quality - 1;
+            }
+
+            item.SellIn = item.SellIn - 1;
+
+            if (item.SellIn < 0)
+            {
+                if (item.Quality > 0)
+                {
+                    item.Quality = item.Quality - 1;
+                }
+            }
+        }
+
+        private static void DoUpdateQualityForSulfuras(Item item)
+        {
+        }
+
+        private static void DoUpdateQualityForBackStagepasses(Item item)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
+
+                if (item.SellIn < 11)
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality = item.Quality + 1;
+                    }
+                }
+
+                if (item.SellIn < 6)
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality = item.Quality + 1;
+                    }
+                }
+            }
+
+            item.SellIn = item.SellIn - 1;
+
+            if (item.SellIn < 0)
+            {
+                item.Quality = 0;
+            }
+        }
+
+        private static void DoUpdateQualityForAgedBrie(Item item)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
+            }
+
+            item.SellIn = item.SellIn - 1;
+
+            if (item.SellIn < 0)
             {
                 if (item.Quality < 50)
                 {
                     item.Quality = item.Quality + 1;
                 }
-
-                item.SellIn = item.SellIn - 1;
-
-                if (item.SellIn < 0)
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-                    }
-                }
-            }
-            else
-            {
-                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-
-                        if (item.SellIn < 11)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-
-                        if (item.SellIn < 6)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-                    }
-
-                    item.SellIn = item.SellIn - 1;
-
-                    if (item.SellIn < 0)
-                    {
-                        item.Quality = 0;
-                    }
-                }
-                else
-                {
-                    if (item.Name == "Sulfuras, Hand of Ragnaros")
-                    {
-                    }
-                    else
-                    {
-                        if (item.Quality > 0)
-                        {
-                            item.Quality = item.Quality - 1;
-                        }
-
-                        item.SellIn = item.SellIn - 1;
-
-                        if (item.SellIn < 0)
-                        {
-                            if (item.Quality > 0)
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
-                        }
-                    }
-                }
             }
         }
+
+        #endregion Item updaters
     }
 }
