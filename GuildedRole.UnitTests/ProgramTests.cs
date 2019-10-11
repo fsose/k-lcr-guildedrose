bc84136a0b035f8bf37ed45ca84c4e078375bfcb
Partho P. Das <partho.das@edmi.com.au>
2019-10-11 11:05:14 +1000

    Step 5 - Add approval tests for Program.cs.

---
+++ GuildedRole.UnitTests/ProgramTests.cs
@@ -0,0 +1,30 @@
+﻿using ApprovalTests;
+using ApprovalTests.Reporters;
+using GuildedRose;
+using System;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace GuildedRole.UnitTests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        [UseReporter(typeof(QuietReporter))]
+        public void ProgramApprovalTest()
+        {
+            var fakeOutput = new StringBuilder();
+            using (var sw = new StringWriter(fakeOutput))
+            using (var sr = new StringReader("a\n"))
+            {
+                Console.SetOut(sw);
+                Console.SetIn(sr);
+
+                Program.Main(new string[] { });
+
+                Approvals.Verify(fakeOutput);
+            }
+        }
+    }
+}
